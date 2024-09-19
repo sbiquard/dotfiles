dfedb9929cd69b06eb63ab2020a93fc8740519b8
Simon Biquard <sbiquard+git@gmail.com>
2024-09-19 22:12:01 +0200

    change to ohmyzsh
    - remove redundant aliases
    - use omz template for zshrc

--- private_dot_config/zsh/aliases.zsh
+++ private_dot_config/zsh/aliases.zsh
@@ -1,20 +1,6 @@
 ###---------- ALIASES ----------###
 
-# navigate
-alias ..="cd .."
-alias ...=" cd ../../"
-alias ....="cd ../../../"
-alias .....="cd ../../../../"
-alias ls="eza"
-alias la="eza -a"
-alias ll="eza -la"
-alias cl="clear"
-
-# zellij
+alias py=python
 alias zj=zellij
-
-# venv
 alias vnv="source .venv/bin/activate"
-
-# lazygit
 alias lg=lazygit
