d9f20a0d39ccdac15dbc49bb12755aee851b76df
Simon Biquard <sbiquard+git@gmail.com>
2024-10-30 10:41:22 +0100

    Yazi wrapper

---
+++ private_dot_config/fish/functions/y.fish
@@ -0,0 +1,9 @@
+# wrapper to be able to cd when exiting Yazi
+function y
+    set tmp (mktemp -t "yazi-cwd.XXXXXX")
+    yazi $argv --cwd-file="$tmp"
+    if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+        builtin cd -- "$cwd"
+    end
+    rm -f -- "$tmp"
+end
