090a0891fb31a36c34f6c24766e32ab4f95d5b9a
Simon Biquard <sbiquard+git@gmail.com>
2024-11-06 08:49:25 +0100

    add .profile and update fish config accordingly

--- private_dot_config/fish/conf.d/env.fish
+++ private_dot_config/fish/conf.d/env.fish
@@ -1,11 +1,3 @@
 # preferred software
 set -gx EDITOR hx
 set -gx BROWSER firefox
-
-# pyenv
-set -gx PYENV_ROOT ~/.pyenv
-
-# TeX
-set -gx --append PATH ~/texlive/2023/bin/x86_64-linux
-set -gx --append MANPATH ~/texlive/2023/texmf-dist/doc/man
-set -gx --append INFOPATH ~/texlive/2023/texmf-dist/doc/info
