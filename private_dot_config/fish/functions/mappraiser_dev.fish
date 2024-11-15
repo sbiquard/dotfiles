e2dc442239915f68dbd8d583193d1210b6d4b417
Simon Biquard <sbiquard+git@gmail.com>
2024-11-15 09:19:47 +0100

    change cmbenv to mappraiser_dev

---
+++ private_dot_config/fish/functions/mappraiser_dev.fish
@@ -0,0 +1,19 @@
+function load_mappraiser
+    set -l install_dir ~/work/software/mappraiser
+    echo "Mappraiser install location is '$install_dir'"
+    set -gx MAPPRAISER_ROOT $install_dir
+    _prepend_paths $install_dir
+    _prepend_pythonpath $install_dir
+    set -l source_dir ~/work/repos/midapack
+    set -l venv_dir $source_dir/.venv
+    echo "Activating virtual environment at '$venv_dir'"
+    source $venv_dir/bin/activate.fish
+    # export PREALPS_ROOT="/home/sbiquard/work/repos/preAlps"
+end
+
+
+function mappraiser_dev
+    module load compiler mpi mkl
+    echo "Loading Mappraiser"
+    load_mappraiser
+end
