aa3165b52af9c9d401d237b6c1284f9154800a31
Simon Biquard <sbiquard+git@gmail.com>
2025-01-19 12:15:12 +0900

    fish: remove some old functions

--- private_dot_config/fish/functions/load_ssparse.fish
+++
@@ -1,4 +0,0 @@
-function load_ssparse
-    set -l dir ~/work/software/ssparse
-    set -gx --prepend LD_LIBRARY_PATH $dir/lib
-end
--- private_dot_config/fish/functions/load_toast.fish
+++
@@ -1,7 +0,0 @@
-function load_toast
-    set -l dir ~/work/software/toast-gcc
-    _prepend_paths $dir
-    pyenv activate toastdev
-    _prepend_pythonpath $dir
-    # load_ssparse
-end
