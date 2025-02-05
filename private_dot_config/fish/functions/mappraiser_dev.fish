c0c905a37a956dcfcc6a6f944859820e1e3098e4
Simon Biquard <sbiquard+git@gmail.com>
2025-02-05 12:04:54 +0100

    activate python venv first in mappraiser_dev function

--- private_dot_config/fish/functions/mappraiser_dev.fish
+++ private_dot_config/fish/functions/mappraiser_dev.fish
@@ -1,12 +1,13 @@
 function mappraiser_dev
+    # activate Python venv
+    set -l venv_dir ~/work/repos/midapack/.venv
+    echo "Activating virtual environment at '$venv_dir'"
+    source $venv_dir/bin/activate.fish
+    # set install location
     set -l install_dir ~/work/software/mappraiser
     echo "Mappraiser install location is '$install_dir'"
     set -gx MAPPRAISER_ROOT $install_dir
     _prepend_paths $install_dir
     _prepend_pythonpath $install_dir
-    set -l source_dir ~/work/repos/midapack
-    set -l venv_dir $source_dir/.venv
-    echo "Activating virtual environment at '$venv_dir'"
-    source $venv_dir/bin/activate.fish
     # export PREALPS_ROOT="/home/sbiquard/work/repos/preAlps"
 end
