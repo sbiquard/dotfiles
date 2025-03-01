07d429f1bdecbcf1d0503539e4c638bc3bad30ad
Simon Biquard <79218427+sbiquard@users.noreply.github.com>
2025-03-01 12:44:27 +0100

    Add fish functions to toggle dark/light theme

---
+++ private_dot_config/fish/functions/dark.fish
@@ -0,0 +1,3 @@
+function dark --description 'Set shell theme to dark mode'
+yes | fish_config theme save "Catppuccin Mocha"
+end
---
+++ private_dot_config/fish/functions/light.fish
@@ -0,0 +1,3 @@
+function light --description 'Set shell theme to light mode'
+yes | fish_config theme save "Catppuccin Latte"
+end
