65db7fb0e2385c8386ab1a6a4f380d08a1f462f9
Simon Biquard <79218427+sbiquard@users.noreply.github.com>
2025-03-14 15:46:06 +0100

    more light-dark stuff
    - change helix and ghostty dark theme to Catppuccin Frappe
    - upgrade fish functions to change helix theme

--- private_dot_config/fish/functions/dark.fish
+++ private_dot_config/fish/functions/dark.fish
@@ -1,3 +1,4 @@
 function dark --description 'Set shell theme to dark mode'
-    yes | fish_config theme save "Catppuccin Mocha"
+    yes | fish_config theme save "Catppuccin Frappe"
+    sed -i 's/theme = .*/theme = "catppuccin_frappe"/' ~/.config/helix/config.toml
 end
--- private_dot_config/fish/functions/light.fish
+++ private_dot_config/fish/functions/light.fish
@@ -1,3 +1,4 @@
 function light --description 'Set shell theme to light mode'
     yes | fish_config theme save "Catppuccin Latte"
+    sed -i 's/theme = .*/theme = "catppuccin_latte"/' ~/.config/helix/config.toml
 end
