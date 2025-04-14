ed4382837bf3e503f2fcef5d72a251b567e09a76
Simon Biquard <sbiquard+git@gmail.com>
2025-04-14 11:34:49 +0200

    fish function to update lazygit

---
+++ private_dot_config/fish/functions/lazygit_update.fish
@@ -0,0 +1,7 @@
+function lazygit_update --description 'Update lazygit to the latest version'
+    set lgversion $(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
+    curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v"$lgversion"/lazygit_"$lgversion"_Linux_x86_64.tar.gz"
+    tar xf lazygit.tar.gz lazygit
+    sudo install lazygit -D -t /usr/local/bin/
+    rm lazygit.tar.gz lazygit
+end
