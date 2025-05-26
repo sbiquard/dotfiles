d573df242fd24445dd75b43bbd8fccd4b778b25a
Simon Biquard <sbiquard+git@gmail.com>
2025-05-26 10:54:26 +0200

    toggle dark and light theme for btop

--- private_dot_config/fish/functions/dark.fish
+++ private_dot_config/fish/functions/dark.fish
@@ -1,4 +1,5 @@
 function dark --description 'Set shell theme to dark mode'
     yes | fish_config theme save "Catppuccin Frappe"
     sed -i 's/theme = .*/theme = "catppuccin_frappe"/' ~/.config/helix/config.toml
+    sed -i 's/color_theme = .*/color_theme = "catppuccin_frappe"/' ~/.config/btop/btop.conf
 end
--- private_dot_config/fish/functions/light.fish
+++ private_dot_config/fish/functions/light.fish
@@ -1,4 +1,5 @@
 function light --description 'Set shell theme to light mode'
     yes | fish_config theme save "Catppuccin Latte"
     sed -i 's/theme = .*/theme = "catppuccin_latte"/' ~/.config/helix/config.toml
+    sed -i 's/color_theme = .*/color_theme = "catppuccin_latte"/' ~/.config/btop/btop.conf
 end
