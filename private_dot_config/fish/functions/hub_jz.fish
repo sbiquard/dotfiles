9ad56643e556269c6e79a3d0f158a72cf180b0f6
Simon Biquard <sbiquard+git@gmail.com>
2025-07-04 17:32:56 +0200

    improve hub_jz function

--- private_dot_config/fish/functions/hub_jz.fish
+++ private_dot_config/fish/functions/hub_jz.fish
@@ -1,8 +1,17 @@
 function hub_jz
     # SSH port forwarding to apcssh gateway
-    # assumes host 'apcssh' is configured in .ssh/config
-    ssh -D localhost:9080 -N -f apcssh
+    # assumes hosts 'apcssh', 'apcssh1', 'apcssh2' and 'apcssh3' are configured in .ssh/config
 
-    # Open browser with jz profile
-    firefox -p jz &
+    # Try the primary gateway first, fall back to alternatives if each fails
+    ssh -D localhost:9080 -N -f -o ConnectTimeout=10 apcssh\
+    || ssh -D localhost:9080 -N -f -o ConnectTimeout=10 apcssh1\
+    || ssh -D localhost:9080 -N -f -o ConnectTimeout=10 apcssh2\
+    || ssh -D localhost:9080 -N -f -o ConnectTimeout=10 apcssh3
+
+    # Open browser with jz profile only if the SSH tunnel was successfully established
+    if test $status -eq 0
+        firefox -p jz &
+    else
+        echo "SSH tunnel setup failed. Not launching browser."
+    end
 end
