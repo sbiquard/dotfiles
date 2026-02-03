13bece26f6fbe6b9c215f20ecf8453087d341eae
Simon Biquard <79218427+sbiquard@users.noreply.github.com>
2026-02-03 09:57:59 +0000

    [fish] update for 4.3

--- private_dot_config/fish/functions/light.fish
+++ private_dot_config/fish/functions/light.fish
@@ -1,5 +1,5 @@
 function light --description 'Set shell theme to light mode'
-    yes | fish_config theme save "Catppuccin Latte"
+    yes | fish_config theme choose "Catppuccin Latte"
     sed -i 's/theme = .*/theme = "catppuccin_latte"/' ~/.config/helix/config.toml
     sed -i 's/color_theme = .*/color_theme = "catppuccin_latte"/' ~/.config/btop/btop.conf
 end
