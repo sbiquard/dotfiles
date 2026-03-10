e6a382ef923078d47fd99eb5411d88b1a538db45
Simon Biquard <79218427+sbiquard@users.noreply.github.com>
2026-03-10 09:39:28 +0000

    [fish] improve lazygit_update

--- private_dot_config/fish/functions/lazygit_update.fish
+++ private_dot_config/fish/functions/lazygit_update.fish
@@ -1,7 +1,28 @@
 function lazygit_update --description 'Update lazygit to the latest version'
-    set lgversion $(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
-    curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v"$lgversion"/lazygit_"$lgversion"_Linux_x86_64.tar.gz"
-    tar xf lazygit.tar.gz lazygit
-    sudo install lazygit -D -t /usr/local/bin/
-    rm lazygit.tar.gz lazygit
+    set lgversion (curl -sf "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
+    if test -z "$lgversion"
+        echo "Error: could not fetch latest version" >&2
+        return 1
+    end
+
+    if not curl -sLo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v$lgversion/lazygit_"$lgversion"_Linux_x86_64.tar.gz"
+        echo "Error: download failed" >&2
+        rm -f lazygit.tar.gz
+        return 1
+    end
+
+    if not tar xf lazygit.tar.gz lazygit
+        echo "Error: extraction failed" >&2
+        rm -f lazygit.tar.gz
+        return 1
+    end
+
+    if not sudo install lazygit -D -t /usr/local/bin/
+        echo "Error: installation failed" >&2
+        rm -f lazygit.tar.gz lazygit
+        return 1
+    end
+
+    rm -f lazygit.tar.gz lazygit
+    echo "lazygit $lgversion installed"
 end
