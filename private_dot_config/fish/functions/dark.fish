eca7b07f02e9a6b745134431b2db6cb7c4f33e3a
Simon Biquard <79218427+sbiquard@users.noreply.github.com>
2026-04-12 22:25:03 +0100

    theme: frappe->mocha

--- private_dot_config/fish/functions/dark.fish
+++ private_dot_config/fish/functions/dark.fish
@@ -1,5 +1,5 @@
 function dark --description 'Set shell theme to dark mode'
-    yes | fish_config theme choose "Catppuccin Frappe"
-    sed -i 's/theme = .*/theme = "catppuccin_frappe"/' ~/.config/helix/config.toml
-    sed -i 's/color_theme = .*/color_theme = "catppuccin_frappe"/' ~/.config/btop/btop.conf
+    yes | fish_config theme choose "Catppuccin Mocha"
+    sed -i 's/theme = .*/theme = "catppuccin_mocha"/' ~/.config/helix/config.toml
+    sed -i 's/color_theme = .*/color_theme = "catppuccin_mocha"/' ~/.config/btop/btop.conf
 end
