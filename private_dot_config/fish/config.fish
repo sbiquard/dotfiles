285d6b882203506bf4ed2b86450f2f2a736428fb
Simon Biquard <79218427+sbiquard@users.noreply.github.com>
2026-04-29 10:39:43 +0100

    [fish] clean up dark/light mode switching

--- private_dot_config/fish/config.fish
+++ private_dot_config/fish/config.fish
@@ -1,3 +1,16 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    fish_config theme choose catppuccin-frappe
+end
+
+# activate this event handler for every new shell
+function _auto_theme --on-variable fish_terminal_color_theme
+    switch $fish_terminal_color_theme
+        case light
+            sed -i 's/theme = .*/theme = "catppuccin_latte"/' ~/.config/helix/config.toml
+            sed -i 's/color_theme = .*/color_theme = "catppuccin_latte"/' ~/.config/btop/btop.conf
+        case dark
+            sed -i 's/theme = .*/theme = "catppuccin_frappe"/' ~/.config/helix/config.toml
+            sed -i 's/color_theme = .*/color_theme = "catppuccin_frappe"/' ~/.config/btop/btop.conf
+    end
 end
--- private_dot_config/fish/functions/dark.fish
+++
@@ -1,5 +0,0 @@
-function dark --description 'Set shell theme to dark mode'
-    yes | fish_config theme choose "Catppuccin Mocha"
-    sed -i 's/theme = .*/theme = "catppuccin_mocha"/' ~/.config/helix/config.toml
-    sed -i 's/color_theme = .*/color_theme = "catppuccin_mocha"/' ~/.config/btop/btop.conf
-end
